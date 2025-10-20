f91a9bb241e5b5c32b536251428035278cfda72e
karungop <karun.gopal@gmail.com>
2025-10-20 16:35:17 -0400

    set up command shift v hotkey. Currently does not do anything, but is able to detect when it is pressed

--- Apple-Clipboard/AppDelegate.swift
+++ Apple-Clipboard/AppDelegate.swift
@@ -7,12 +7,17 @@
 
 import AppKit
 import SwiftUI
+import Carbon
 
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem!
     var clipboardWatcher: ClipboardWatcher!
     var clipboardHistory: [String] = []
+    
+    private var hotKeyRef: EventHotKeyRef? = nil
+    private var eventHandlerRef: EventHandlerRef? = nil
+
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
@@ -23,6 +28,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         menu.addItem(.separator())
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q"))
         statusItem.menu = menu
+        
+        var eventType = EventTypeSpec(eventClass: OSType(kEventClassKeyboard),eventKind: UInt32(kEventHotKeyPressed))
+        let userData = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+        let installStatus = InstallEventHandler(GetApplicationEventTarget(),
+                                                    hotKeyHandler,
+                                                    1,
+                                                    &eventType,
+                                                    userData,
+                                                    &eventHandlerRef)
+        if installStatus != noErr {
+            print("Failed to install event handler: \(installStatus)")
+        }
+        
+        let vKeyCode: UInt32 = UInt32(kVK_ANSI_V)
+        let modifiers: UInt32 = UInt32(cmdKey) | UInt32(shiftKey)
+        var hotKeyID = EventHotKeyID(signature: OSType(0), id: UInt32(1))
+        
+        let registerStatus = RegisterEventHotKey(vKeyCode,
+                                                     modifiers,
+                                                     hotKeyID,
+                                                     GetApplicationEventTarget(),
+                                                     0,
+                                                     &hotKeyRef)
+        
+        if registerStatus != noErr {
+            print("Failed to register hotkey: \(registerStatus)")
+        } else {
+            print("Hotkey registered: ⌘ + Shift + V")
+        }
 
         clipboardWatcher = ClipboardWatcher()
         clipboardWatcher.onNewCopy = { [weak self] text in
@@ -30,6 +64,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         clipboardWatcher.startWatching()
     }
+    
+    func handleHotkeyPressed() {
+        print("Hotkey pressed")
+//        showClipboard()
+    }
+    
+    private let hotKeyHandler: EventHandlerUPP = { (nextHandlerPointer, eventRefPointer, userDataPointer) -> OSStatus in
+        guard let event = eventRefPointer else { return noErr }
+        
+        var hotKeyID = EventHotKeyID()
+            let size = UInt32(MemoryLayout<EventHotKeyID>.size)
+
+            // Get the EventHotKeyID from the event's direct object parameter
+            let status = GetEventParameter(event,
+                                           EventParamName(kEventParamDirectObject),
+                                           EventParamType(typeEventHotKeyID),
+                                           nil,
+                                           Int(size),
+                                           nil,
+                                           &hotKeyID)
+            if status != noErr {
+                return status
+            }
+
+            // hotKeyID.id will be the id we used when registering (1)
+            if hotKeyID.id == 1 {
+                if let userData = userDataPointer {
+                    // Recover the AppDelegate instance
+                    let delegate = Unmanaged<AppDelegate>.fromOpaque(userData).takeUnretainedValue()
+                    // Call instance method on the main thread
+                    DispatchQueue.main.async {
+                        delegate.handleHotkeyPressed()
+                    }
+                }
+            }
+
+            return noErr
+        }
     
 
     private func addToHistory(_ text: String) {
@@ -72,6 +144,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @objc private func quitApp() {
+        if let hk = hotKeyRef {
+            UnregisterEventHotKey(hk)
+            hotKeyRef = nil
+        }
+        if let handler = eventHandlerRef {
+            RemoveEventHandler(handler)
+            eventHandlerRef = nil
+        }
+        print("Unregistered custome hotkey")
         NSApp.terminate(nil)
     }
 }
